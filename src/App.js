66f35be079e17ed946c0d503903cc0cc7e712f58
Chany97 <chanhwui@ajou.ac.kr>
2021-12-03 22:20:04 +0900

    show video list with title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAtrSZkopq--QXlpEYQ5SrM9Kg5TZlZMl0',
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
   return (
     <>
-      <h1>hello :)</h1>
+      <VideoList videos={videos}/>
     </>
   );
 }
