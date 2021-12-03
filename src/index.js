bd0786a41c862f80c0ae0b4e0455d8dd4918a2b0
Chany97 <chanhwui@ajou.ac.kr>
2021-12-03 23:31:37 +0900

    youtube service

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import '@fortawesome/fontawesome-free/js/all.js';
+import Youtube from './service/youtube';
 
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 ReactDOM.render(
   <>
-    <App />
+    <App youtube={youtube}/>
   </>,
   document.getElementById('root')
 );
